2cd835f6054d2c2ec72c4838208105863befe853
tupe12334 <ofek.gabay.he@gmail.com>
2023-10-20 09:22:03 +0300

    feat: add file name

--- index.ts
+++ index.ts
@@ -5,9 +5,9 @@ if (module === require.main) {
   (async () => {
     const jsonFile = await readFile("./data.json", "utf-8");
     const jsonData = JSON.parse(jsonFile);
-
+    const name = jsonData[0].name;
     const kmlData = convertJsonToKml(jsonData[0]);
-    await writeFile("./data.kml", kmlData);
+    await writeFile(`./${name}.kml`, kmlData);
     console.log("KML file written");
   })();
 }
